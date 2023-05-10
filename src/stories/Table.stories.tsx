495176d14c8c62a4d08f87be02e4f88b71136b7d
developeranku <ankur4736@gmail.com>
2023-05-10 17:22:59 +0530

    added storybook

---
+++ src/stories/Table.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import Table from '../index';
+import { users } from './mocks/data';
+
+const meta: Meta<typeof Table> = {
+  title: 'Table',
+  component: Table,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Table>;
+
+const getHeaders = (prospects:any) => {
+  const arr:any[] = [];
+  Object.keys(prospects[0]).map((k) => arr.push({
+    fieldName: k,
+    headerName: k.toUpperCase(),
+    isVisible: true,
+  }));
+  return arr;
+};
+
+function StoryComp() {
+  const [prospects] = React.useState(users);
+
+  if (!prospects.length) return null;
+  return (
+    <div>
+      <Table
+        headers={getHeaders(prospects)}
+        rows={prospects}
+        showPageHeader
+        showToolbar
+        docTitle="My document"
+      />
+    </div>
+  );
+}
+
+export const DatasheetTable:Story = {
+  render: () => <StoryComp />,
+};
+
+export const parameters = { layout: 'fullscreen' };
