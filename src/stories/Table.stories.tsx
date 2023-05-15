e5debcc29942d06e5da92c79d7185b90cbd94fe3
Ankur Sharma <ankur4736@gmail.com>
2023-05-15 16:30:21 +0530

    added data presets for testing (#17)

--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import Table from '../index';
-import randomData from './mocks/200-cols.json';
+import twoHundredCols from './mocks/200-cols.json';
+import fourHundredCols from './mocks/400-cols.json';
+import sixHundredCols from './mocks/600-cols.json';
+import oneThousandCols from './mocks/1000-cols.json';
 
 const meta: Meta<typeof Table> = {
   title: 'Table',
@@ -22,19 +25,65 @@ const getHeaders = (prospects:any) => {
   return arr;
 };
 
-function StoryComp() {
-  const [prospects] = React.useState(randomData);
+type IPresetData = '200cols' | '400cols' | '600cols' | '1000cols' ;
 
-  if (!prospects.length) return null;
+const getProspects = (presetData:IPresetData) => {
+  if (presetData === '200cols') return twoHundredCols;
+  if (presetData === '400cols') return fourHundredCols;
+  if (presetData === '600cols') return sixHundredCols;
+  if (presetData === '1000cols') return oneThousandCols;
+  return null;
+};
+
+function StoryComp() {
+  const [presetData, setPresetData] = React.useState<IPresetData | null>(null);
+  const prospects = getProspects(presetData as any);
+
+  if (!presetData) {
+    return (
+      <div style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '20px',
+        marginBottom: '20px',
+      }}
+      >
+        <small>Please selected one of preset for sample data: &nbsp;</small>
+        <select onChange={(e) => setPresetData(e.target.value as IPresetData)}>
+          <option value="">
+            Choose option
+          </option>
+          <option value="200cols">
+            200 Columns
+          </option>
+          <option value="400cols">
+            400 Columns
+          </option>
+          <option value="600cols">
+            600 Columns
+          </option>
+          <option value="1000cols">
+            1000 Columns
+          </option>
+        </select>
+      </div>
+    );
+  }
+
   return (
     <div>
+      {presetData && (
       <Table
         headers={getHeaders(prospects)}
         rows={prospects}
         showPageHeader
         showToolbar
         docTitle="My document"
+        key={presetData}
       />
+      )}
     </div>
   );
 }
